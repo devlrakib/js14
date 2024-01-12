d0dc8bc2fbd1778c3ee53263f557c2c555ace792
devlrakib <devrakib5@gmail.com>
2024-01-13 00:11:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+ var num = prompt("Enter A Number");
+ if(num>=80)
+ console.log("A+");
+
+ else if (num>=70)
+console.log("A");
+
+else if (num>=60)
+console.log("A-");
+
+else if (num>=50)
+console.log("B");
+
+else if(num>=40)
+console.log("C");
+
+else
+console.log("Fail")
+
+
+
